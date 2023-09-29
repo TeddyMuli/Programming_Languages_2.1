7406eeb94506d6a9725a846f356a8993965a7eb1
TeddyMuli <tmuli974@gmail.com>
2023-09-29 07:25:39 +0300

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Teddy Muli Ndaisi         SCT211-0023/2022
+"""Program that takes input of two numbers, calculates the sum and prints it to the screen"""
+print("*****Calculator*****")
+name = input("Hi, enter your name > ")
+num1 = int(input("Enter the first number > "))
+num2 = int(input("Enter the second number > "))
+
+sum = num1 + num2
+
+print(f"Hi {name} the sum of {num1} and {num2} is {sum}")
