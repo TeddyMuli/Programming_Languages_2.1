7c99442fb44d401ae530d697453f95cc6cabc4d8
TeddyMuli <tmuli974@gmail.com>
2023-10-06 19:12:33 +0300

    assignment 2

--- assignment_2/calculator_2.py
+++ assignment_2/calculator_2.py
@@ -10,13 +10,13 @@ birthdate = datetime.datetime.strptime(date_of_birth, '%d/%m/%Y')
 
 age = todays_date - birthdate
 
-years = ((age) / (365.242))
+years = ((age.total_seconds()) / (365.242190*24*3600))
 years_int = int(years)
 
 months = (years - years_int) * 12
 months_int = int(months)
 
-days = (months - months_int) * (365.242/12)
+days = (months - months_int) * (365.242190/12)
 days_int = int(days)
 
 print(f"Hi {name}, you are {years_int} years, {months_int} months and {days_int} days old.")
--- assignment_2/leap_year.py
+++ assignment_2/leap_year.py
@@ -2,7 +2,10 @@
 
 year = int(input("Enter the year > "))
 
-if (year % 4 == 0):
-    print(f"{year} is a leap year.")
+if ((year % 100 == 0) and (year % 400 != 0)):
+        print(f"{year} is not a leap year")
 else:
-    print(f"{year} is not a leap year.")
+    if (year % 4 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
